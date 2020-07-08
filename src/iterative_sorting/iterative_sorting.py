dd58016de02f2ae748682307b09cb5cd55770198
Zernach <ryan@zernach.com>
2020-07-08 19:55:36 -0400

    solves selection_sort and bubble_sort functions

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,23 +2,25 @@
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
-
-        # TO-DO: swap
-        # Your code here
+        tracker = i
+        smallest_index = tracker
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
-
+    had_swaps = True
+    while had_swaps == True:
+        had_swaps = False
+        for i in range(0, len(arr) - 1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                had_swaps = True
     return arr
 
 '''
