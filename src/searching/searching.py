bd6fad2045c9c2ffd946995da02494c6c6b27b4f
Zernach <ryan@zernach.com>
2020-07-08 20:54:25 -0400

    adds searching solutions with tests passed

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,26 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    if len(arr) == 0:
+        return -1  # not found b/c array is empty
 
+    low = 0
+    high = len(arr)-1
 
+    while low < high:
+        mid = (low + high)//2
+        if target == arr[mid]:
+            return mid
+        elif target > arr[mid]:
+            low = mid + 1
+        elif target < arr[mid]:
+            high = mid
+            
     return -1  # not found
